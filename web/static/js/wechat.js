52a57c553da84bd3abad5834014e06370f40a20b
Soar <85624529@qq.com>
2016-10-13 02:38:18 +0800

    bug try share

--- web/static/js/wechat.js
+++ web/static/js/wechat.js
@@ -22,21 +22,23 @@ $(function(){
         'onMenuShareQQ'
       ]
     });
+
+    wx.onMenuShareAppMessage({
+      title: 'hah', // 分享标题
+      desc: 'woshi', // 分享描述
+      link: window.location, // 分享链接
+      imgUrl: '', // 分享图标
+      type: '', // 分享类型,music、video或link，不填默认为link
+      dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+      success: function () { 
+          // 用户确认分享后执行的回调函数
+        alert("ffff");
+      },
+      cancel: function () { 
+          // 用户取消分享后执行的回调函数
+      }
+    });
+
   })
-  wx.onMenuShareAppMessage({
-    title: 'hah', // 分享标题
-    desc: 'woshi', // 分享描述
-    link: window.location, // 分享链接
-    imgUrl: '', // 分享图标
-    type: '', // 分享类型,music、video或link，不填默认为link
-    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-    success: function () { 
-        // 用户确认分享后执行的回调函数
-      alert("ffff");
-    },
-    cancel: function () { 
-        // 用户取消分享后执行的回调函数
-    }
-  });
 })
 
